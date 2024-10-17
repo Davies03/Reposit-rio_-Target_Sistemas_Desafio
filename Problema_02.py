e772560590ec6061327d5b21524a0fcdf51bb011
Davi B Pereira <dbatistapereira003@gmail.com>
2024-10-17 01:36:53 -0300

    Update Problema_02.py

--- Problema_02.py
+++ Problema_02.py
@@ -1,11 +1,14 @@
+""" 2) Escreva um programa que verifique, em uma string, a existência da letra ‘a’, seja maiúscula ou minúscula,
+além de informar a quantidade de vezes em que ela ocorre."""
+
 def contar_letra_a(texto):
-    """Conta a quantidade de vezes que a letra 'a' (maiúscula e minúscula) aparece em uma palavra."""
+    """Conta a quantidade de vezes que a letra 'a' (maiúscula e minúscula) aparece em uma String."""
     return texto.lower().count('a')
 
 # Entrada do usuário
 texto = input("Escreva uma palavra: ")
 
-# Contar a letra 'a' na palavra
+# Contar a letra 'a' na string
 quantidade_a = contar_letra_a(texto)
 
 # Exibir os resultados
@@ -18,8 +21,8 @@ else:
 """Explicação do Código:"""
 
 """1.Função contar_letra_a(texto):
- * Converte a Palavra para minúsculas usando lower() para garantir que tanto 'a' quanto 'A' sejam contados.
- * Utiliza o método count() para contar quantas vezes a letra 'a' aparece na Palavra."""
+ * Converte a String para minúsculas usando lower() para garantir que tanto 'a' quanto 'A' sejam contados.
+ * Utiliza o método count() para contar quantas vezes a letra 'a' aparece na String."""
 
 """2.Entrada do Usuário:
  * O programa pede ao usuário para inserir uma Palavra."""
